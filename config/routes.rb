c3d4ef8d99c16555f5cc9cfff11c8cdd3da179cb
Aaron Groch <ar.groch@gmail.com>
2020-12-25 20:40:26 -0800

    working getting Action Cable to work on Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root 'home#index'
   resources :messages, only: [:new, :create]
   resources :rooms, only: [:new, :create, :show]
+  # mount ActionCable.server => '/cable'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
