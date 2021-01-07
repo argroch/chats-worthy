e1733e88f1769a7ad7740b5d5e81fa936324f1fd
Aaron Groch <ar.groch@gmail.com>
2021-01-07 09:27:42 -0800

    worked on styling the Room index and the Rooms themselves

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -13,11 +13,10 @@ consumer.subscriptions.create("RoomChannel", {
 
   received(data) {
   	if(data.room == $('#room_messages').attr('data-room-id')){
-    	$('#room_messages').append('<div class="message">' + data.user + ': ' + data.content + '</div>');
+    	$('#room_messages').append('<div class="row"><div class="col-md-1"><i class="fas fa-portrait fa-4x"></i></div><div class="message"><p><strong>' + data.user + '</strong> <small>' + data.timestamp + '</small></p><p>' + data.content + '</p></div></div>');
     }
   }
 })
-
 
 let submit_messages;
 
